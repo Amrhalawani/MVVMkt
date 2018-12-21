421774712b9298850ff841fe51007ee13bc6b24c
Amrhal <eng.amrhalawani@rocketmail.com>
2018-12-21 06:32:14 +0200

    initial Commit

--- app/src/main/java/com/amrhal/mvvm/FakeRoomDatabase/FakeDatabase.kt
+++ app/src/main/java/com/amrhal/mvvm/FakeRoomDatabase/FakeDatabase.kt
@@ -10,9 +10,9 @@ class FakeDatabase private constructor(){
         @Volatile private var instance: FakeDatabase? = null // volatile means that writes to this field are immediately made visible to other threads.
 
         fun getInstance() = instance ?: synchronized(this){ //elvis operator if instance equal null will use the rest of code
-        instance ?: FakeDatabase().also { instance = it
-        } // "it" means the reference instance something like "this"
-    }
+            instance ?: FakeDatabase().also { instance = it
+            } // "it" means the reference instance something like "this"
+        }
     }
 
 
